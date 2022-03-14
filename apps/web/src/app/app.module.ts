bcb07f98c3303bcec92c48469eb0c946872f060b
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-03-14 00:17:52 -0500

    feat(web): add nswag

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { API_BASE_URL } from 'libs/web/shared-data-access-api-sdk/src';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [{ provide: API_BASE_URL, useValue: environment.apiBaseUrl }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ libs/web/shared-data-access-api-sdk/README.md
@@ -0,0 +1,3 @@
+# web-shared-data-access-api-sdk
+
+This library was generated with [Nx](https://nx.dev).
